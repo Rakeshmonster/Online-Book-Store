cb4e5816662df45defca3c484942127220f5b9cb
Rakesh Rawal <rakeshrawal062@gmail.com>
2025-03-26 09:58:33 +0545

    done

---
+++ footer.php
@@ -0,0 +1,32 @@
+<section class="footer">
+  <div class="footer_box_container">
+    <div class="footer_box">
+      <div class="footer_logo_cont">
+        <img src="book_logo.png" alt="">
+      <a href="home.php" class="book_logo">Dahal Pustal Pasal</a>
+
+      </div>
+      <p><i class="fas fa-phone"></i> 9868587583</p>
+      <p><i class="fas fa-envelope"></i> dahalpustakpasal@gmail.com</p>
+      <p><i class="fas fa-map-marker-alt"></i> Dhangadhi-2, Nepal</p>
+      <p><i class="fa-solid fa-shop"></i> Shop Timings : 24/7</p>
+    </div>
+
+    <div class="footer_box">
+      <h3>Quick Links</h3>
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="shop.php">Shop</a>
+      <a href="contact.php">Contact</a>
+    </div>
+
+    <div class="footer_box">
+      <h3>Other Links</h3>
+      <a href="login.php">Login</a>
+      <a href="register.php">Register</a>
+      <a href="cart.php">Cart</a>
+      <a href="orders.php">Orders</a>
+    </div>
+  </div>
+  <p>Copyright <i class="fa-regular fa-copyright"></i> 2024 <span>Rakesh Amin | All Rights Reserved.</span></p>
+</section>
